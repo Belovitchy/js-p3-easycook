978a4ac1ceba09cedfca54f1433fe12baf1018d9
Belovitchy <belo38690@gmail.com>
2025-07-17 15:01:43 +0200

    addConsoleLog

--- server/src/main.ts
+++ server/src/main.ts
@@ -20,3 +20,8 @@ app
   .on("error", (err: Error) => {
     console.error("Error:", err.message);
   });
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "✅ OK" : "❌ MISSING");
